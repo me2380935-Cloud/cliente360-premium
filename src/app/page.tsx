99fce05d3d6e625d56a896d7fa68907b8cb60fac
me2380935-Cloud <me2380935@gmail.com>
2025-11-20 17:52:52 -0300

    Atualizar o page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,7 @@
-  const [activeConfigPage, setActiveConfigPage] = useState(null);
+  "use client"
+import React, { useState, useEffect } from "react";
+// outros imports...
+const [activeConfigPage, setActiveConfigPage] = useState(null);
   const [vigilanteData, setVigilanteData] = useState({
     nomeFantasia: '',
     segmentoMercado: '',
